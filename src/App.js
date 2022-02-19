982d2f2956d58d277c5714dbdf6d673aa446b9d2
Nhien Dang <danghaonhien@gmail.com>
2022-02-18 19:59:53 -0800

    added AboutMe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import Home from "./PortfolioContainer/Home/Home";
 import "./App.css";
+import PortfolioContainer from "./PortfolioContainer/PortfolioContainer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className='App'>
-      <Home />
+      <ToastContainer />
+      <PortfolioContainer />
     </div>
   );
 }
